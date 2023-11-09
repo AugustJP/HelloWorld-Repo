9e21072e76afd224c89b1f460e7318974c4bb5f3
August Peterson <143127284+AugustJP@users.noreply.github.com>
2023-11-09 14:32:40 -0600

    Add files via upload

---
+++ August_Peterson_Lab13.py
@@ -0,0 +1,19 @@
+class RoboFriend:
+	
+	
+	
+	def __init__(self, _name, _age):
+		self.name = _name
+		self.age = _age
+		
+	
+	def stateName(self):
+		return(f"Hello. My name is {self.name}.")
+		
+		
+		
+		
+	def stateAge(self):
+		return(f"I'm {self.age} years old")
+		
+	
---
+++ MyProgram.py
@@ -0,0 +1,26 @@
+"""
+#August Peterson
+#11/9/23
+
+#Creates 3 robots and gives them attributes
+"""
+
+from August_Peterson_Lab13 import *
+
+
+Robot1 = RoboFriend("Bob", 104)
+
+Robot2 = RoboFriend("Hubert", 13)
+
+Robot3 = RoboFriend("Gary", 47)
+
+
+
+print(Robot1.stateName())
+print(Robot1.stateAge())
+
+print(Robot2.stateName())
+print(Robot2.stateAge())
+
+print(Robot3.stateName())
+print(Robot3.stateAge())
